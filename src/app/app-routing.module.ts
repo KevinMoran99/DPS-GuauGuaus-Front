1894abdb75aeb97c99b547c28272f0d04b34b981
fer234 <fernandoreyes98@outlook.es>
2020-09-27 14:59:58 -0600

    Se agrega el sidenav, tambien se crean 6 componentes para poder tener navegabilidad con el sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CitasComponent } from './components/citas/citas.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EspeciesComponent } from './components/especies/especies.component';
+import { PermisosComponent } from './components/permisos/permisos.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'citas', component: CitasComponent},
+  {path: 'usuarios', component: UsuariosComponent},
+  {path: 'permisos', component: PermisosComponent},
+  {path: 'especies', component: EspeciesComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,59 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular material
+import {SidenavComponent } from './components/sidenav/sidenav.component';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CitasComponent } from './components/citas/citas.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { PermisosComponent } from './components/permisos/permisos.component';
+import { EspeciesComponent } from './components/especies/especies.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    DashboardComponent,
+    CitasComponent,
+    UsuariosComponent,
+    PermisosComponent,
+    EspeciesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatMenuModule,
+    MatSnackBarModule,
+    MatSlideToggleModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
